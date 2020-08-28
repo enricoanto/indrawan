30fc5bdb809d82a68b71e6a1f7eaa5de5bf27f7d
enricoanto <enricoanto@yahoo.com>
2020-08-28 16:53:03 +0700

    Enrico Dwidhanto Indrawan

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+let score = [3,1,1,2,1,3,0,4,2,5,1,2,1,1,1,1,1,0,1,3]
+let 
+
+for (let a = 0; a < score.length; a++) {{
+        if (score[a] && score[a+10]) {
+            result +=1
+        }
+    }
+}
+console.log(document.getElementsByTagName ("input"))
